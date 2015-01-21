31299834701d2127be5e016b5f2b6cd8526d3e62
Roger Stringer <roger@freekrai.net>
2015-01-20 20:39:36 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# kudos
+# Kudos for your site
+
 Kudos for your site and blogs powered by Data McFly
+
+The idea is to give people Kudo support for their site or blog. Originally Kudos have been introduced by the [Svbtle blog](https://svbtle.com/) engine.
+
+## Installation
+
+Follow these steps to setup kudos for your site.
+
+**Step 1 - Data McFly account**  
+
+This kudo script uses Data McFly as the backend data store. As such, you first need to create a [Data McFly account](https://datamcfly.com/) and create a new app. You can obviously also reuse an existing one if you want so.
+
+**Step 2 - Configure Data McFly**  
+
+Open `kudos.datamcfly.js` and change the  `YOUR-API-KEY`, `YOUR-APP` and `kudos` to have it point to the account you just created in step 1.
+
+```javascript
+...
+// replace this url with yours!!
+var datamcflyKudos = new DataMcFly("YOUR-API-KEY", "YOUR-APP", "kudos");
+    ...
+...
+```
+
+
+**Step 3 - Include kudo scripts**  
+Include the kudo scripts from the `dist` directory
+
+```html
+<!-- Kudos script -->
+<script src="https://cdn.datamcfly.com/DataMcFly.js?latest=1"></script>
+<script type="text/javascript" src="kudos/kudos.js"></script>
+<script type="text/javascript" src="kudos/kudos.datamcfly.js"></script>
+<link rel="stylesheet" href="kudos/kudos.css">
+```
+
+Note that you need to have jQuery as well.
+
+**Step 4 - Add HTML snippet**  
+As the last step include the following snippet
+
+```html
+<figure class="kudo"></figure>
+```
+
+in your DOM where you'd like to have the kudo element appear and initialize it by invoking
+
+```javascript
+$('figure.kudo').kudoable({
+	dataStore: window.datamcflyStorage
+});
+```
+
+That's it. You should be ready to go.
+
+## Issues, Bugs, Enhancements..
+
+Simply use [GitHub's issues](https://github.com/DataMcFly/kudos/issues). PRs are welcome as well.
+
+## Credits
+
+Credits have to be given to [Amit Upadhyay](http://amitu.com/2013/04/kudos-using-parse-for-jekyll/) who provided the original Svbtle independent Kudo implementation by using [Parse](https://parse.com/) as the backend service.
---
+++ kudos/kudos.datamcfly.js
@@ -0,0 +1,92 @@
+(function(exports, undefined){
+
+    // replace this api info with yours!!
+	var datamcflyKudos = new DataMcFly("YOUR-API-KEY", "YOUR-APP", "kudos");
+	var key = document.location.pathname.replace(/[\/-]/g,'');
+
+    // fix for local debugging
+    if(key === ''){
+        key = 'localhost'
+    }
+	
+	function createUUID() {
+		var s = [];
+		var hexDigits = "0123456789abcdef";
+		for (var i = 0; i < 36; i++) {
+			s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+		}
+		s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+		s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+		s[8] = s[13] = s[18] = s[23] = "-";
+		
+		var uuid = s.join("");
+		return uuid;
+	}
+	
+	function getAuthData(){
+		if( localStorage.getItem("uuid") === null) {
+			var uuid = createUUID();
+			localStorage.setItem("uuid",uuid);
+			return uuid;
+		}else{
+			return localStorage.getItem("uuid");
+		}
+	}
+	var uid = getAuthData();
+
+    var hasVoted = function(){
+		var deferred = $.Deferred();
+		datamcflyKudos.where({'key' : key,'uid' : uid}).limit(1).on('value', function(data){
+			deferred.resolve( data.count() !== null);
+		});
+		return deferred.promise();
+	};
+
+    var addKudo = function(){
+		datamcflyKudos.set({
+			'key' : key,
+			'uid' : uid,
+			'likes' : 1
+		});
+	};
+	
+    var removeKudo = function(){
+		datamcflyKudos.where({'key' : key,'uid' : uid}).on('value', function(data){
+			if( data.count() ){
+				data.forEach( function(snapshot){
+					var doc = snapshot.value();
+					datamcflyKudos.deleteDocument( doc._id );
+				});
+			}
+		});
+	};
+
+    // listening for updates
+    var onKudoUpdates = function(cb){
+		var likeCount = 0;
+		datamcflyKudos.where({'key' : key}).on('value', function(data){
+			likeCount = data.count();
+			cb( likeCount );
+		});
+
+		datamcflyKudos.on('added', function(data){
+			likeCount = likeCount + 1;
+			cb( likeCount );
+		});
+
+		datamcflyKudos.on('removed', function(data){
+			likeCount = likeCount - 1;
+			cb( likeCount );
+		});
+    };
+
+    var datamcflyStorage = {
+        hasVoted: hasVoted,
+        addKudo: addKudo,
+        removeKudo: removeKudo,
+        onKudoUpdates: onKudoUpdates
+    };
+
+    exports.datamcflyStorage = datamcflyStorage;
+
+})(window);
